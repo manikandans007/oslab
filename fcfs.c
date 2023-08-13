ccebe0446253ee9748ceec4d98ec8ab9eabb47f2
MANIKANDAN S <102538684+manikandans007@users.noreply.github.com>
2023-08-13 22:19:11 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,58 +1,51 @@
-#include<stdio.h>
-struct Process {
- int pid; // Process ID
- int burst_time; // Burst time
- int at; 
- int waittime;
-};
-void priorityScheduling(struct Process proc[], 
-int n) {
- int i, j;
- struct Process temp;
- for (i = 0; i < n - 1; i++) {
- for (j = 0; j < n - i - 1; j++) {
- if (proc[j].at > proc[j + 1].at) {
- // Swap the processes
- temp = proc[j];
- proc[j] = proc[j + 1];
- proc[j + 1] = temp;
- } } }
- int total_waiting_time = 0;
- int total_turnaround_time = 
-proc[0].burst_time;
- proc[0].waittime=0;
- 
- for (i = 1; i < n; i++) {
- proc[i].waittime=proc[i1].waittime+proc[i-1].burst_time;
- 
-total_turnaround_time+=proc[i].waittime+pro
-c[i].burst_time;
- total_waiting_time+=proc[i].waittime;
- }
- printf("Process\tBurst Time\tArrival 
-Time\tWaiting Time \t Turn around time\n");
- for (i = 0; i < n; i++) {
- printf("%d\t%d\t\t%d\t%d\t%d\n", 
-proc[i].pid, proc[i].burst_time, 
-proc[i].at,proc[i].waittime,proc[i].waittime+pr
-oc[i].burst_time);
- }
- printf("\nAverage Waiting Time: %.2f\n", 
-(float) total_waiting_time / n);
- printf("Average Turnaround Time: %.2f\n", 
-(float) total_turnaround_time / n);
-}
-void main() {
- int n, i;
- printf("Enter the number of processes: ");
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+ int pid;
+ int btime;
+ int atime;
+ int wtime;
+ int tatime;
+} sp;
+int main(){
+ int i, j, n,ctime=0;
+ float total_waiting_time=0, total_turnaround_time=0;
+ sp *p, t;
+ printf("Enter the no. of processes: ");
  scanf("%d", &n);
-struct Process proc[n];
- printf("Enter process details (Process ID, 
-Burst Time, Arrival Time):\n");
- for (i = 0; i < n; i++) {
- printf("Process %d: ", i + 1);
- scanf("%d %d %d", &proc[i].pid, 
-&proc[i].burst_time, &proc[i].at);
+ p = (sp *)malloc(n * sizeof(sp));
+ printf("Enter the burst time and arrival time of");
+ for (i = 0; i < n; i++){
+ printf("\nProcess%d\t:", i + 1);
+ scanf("%d", &p[i].btime);
+ scanf("%d", &p[i].atime);
+ p[i].pid = i + 1;
+ p[i].wtime = 0;
  }
- priorityScheduling(proc, n);
+ for(i=0;i<n;i++){
+ for(j=0;j<n-i-1;j++){
+ if(p[j].atime>p[j+1].atime){
+ t=p[j];
+ p[j]=p[j+1];
+ p[j+1]=t;
+ }
+ }
+ }
+ printf("\n|------------------------------Process Scheduling--------------------------------|");
+ printf("\n|Process\t|Burst Time\t|Arrival Time\t|Waiting Time\t|Turn Around Time|");
+ printf("\n|--------------------------------------------------------------------------------|");
+ for (i = 0; i < n; i++){
+ ctime += p[i].btime;
+ p[i].tatime = ctime - p[i].atime;
+ p[i].wtime = p[i].tatime - p[i].btime;
+ total_waiting_time += p[i].wtime;
+ total_turnaround_time += p[i].tatime;
+ printf("\n|%d\t\t|%d\t\t|%d", p[i].pid, p[i].btime, p[i].atime);
+ printf("\t\t|%d\t\t|%d \t\t |", p[i].wtime, p[i].tatime);
+ }
+ printf("\n|--------------------------------------------------------------------------------|");
+ printf("\n Total Waiting Time: %.2f", total_waiting_time);
+ printf("\n Average Waiting Time: %.2f", (float)total_waiting_time / n);
+ printf("\n Total Turn Around Time: %.2f", total_turnaround_time);
+ printf("\n Average Turn Around Time: %.2f", (float)total_turnaround_time / n);
 }
