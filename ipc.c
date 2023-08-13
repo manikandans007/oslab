ccdad890abc5214066e4d21e527398305683615f
MANIKANDAN S <102538684+manikandans007@users.noreply.github.com>
2023-08-13 21:40:57 +0530

    Update ipc.c

--- ipc.c
+++ ipc.c
@@ -1,46 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
-#include<sys/types.h>
-#define SEGSIZE 100
-int main(int argc, char *argv[ ])
-{
-int shmid,cntr;
-key_t key;
-char *segptr;
-char buff[]="poooda......";
-key=ftok(".",'s');
-if((shmid=shmget(key, SEGSIZE, IPC_CREAT | IPC_EXCL | 0666))== -1)
-{
-if((shmid=shmget(key,SEGSIZE,0))==-1)
-{
-perror("shmget");
-exit(1);
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/shm.h> 
+#include<string.h> 
+int main() 
+{ int i; void *shared_memory; 
+char buff[100]; int shmid; 
+shmid=shmget((key_t)2345, 1024, 0666|IPC_CREAT); 
+printf("Key of shared memory is %d\n",shmid); 
+shared_memory=shmat(shmid,NULL,0); 
+printf("Process attached at %p\n",shared_memory); 
+printf("Enter some data to write to shared memory\n"); 
+gets(buff);
+strcpy(shared_memory,buff); 
+printf("You wrote : %s\n",(char *)shared_memory); 
 }
-}
-else
-{
-printf("Creating a new shared memory seg \n");
-printf("SHMID:%d",shmid);
-}
-system("ipcs –m");
-if((segptr=(char*)shmat(shmid,0,0))==(char*)-1)
-{
-perror("shmat");
-exit(1);
-}
-printf("Writing data to shared memory…\n");
-strcpy(segptr,buff);
-printf("DONE\n");
-printf("Reading data from shared memory…\n");
-printf("DATA:-%s\n",segptr);
-printf("DONE\n");
-printf("Removing shared memory Segment…\n");
-if(shmctl(shmid,IPC_RMID,0)== -1)
-printf("Can‟t Remove Shared memory Segment…\n");
-else
-printf("Removed Successfully");
+Read.c
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/shm.h> 
+#include<string.h> 
+int main() 
+{ int i; void *shared_memory; char buff[100]; 
+int shmid; 
+shmid=shmget((key_t)2345, 1024, 0666); 
+printf("Key of shared memory is %d\n",shmid); 
+shared_memory=shmat(shmid,NULL,0); //process attached to shared memory segment 
+printf("Process attached at %p\n",shared_memory); 
+printf("Data read from shared memory is : %s\n",(char *)shared_memory); 
 }
