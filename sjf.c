c77eee4efa024e2d4dd2dddbb320546c188e7e8d
MANIKANDAN S <102538684+manikandans007@users.noreply.github.com>
2023-08-13 22:15:29 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,58 +1,53 @@
-#include<stdio.h>
-struct Process {
- int pid; // Process ID
- int burst_time; // Burst time
- int waittime;
-};
-void sjfScheduling(struct Process proc[], int n) 
-{
- int i, j;
- struct Process temp;
- for (i = 0; i < n - 1; i++) {
- for (j = 0; j < n - i - 1; j++) {
- if (proc[j].burst_time > proc[j + 
-1].burst_time) {
- // Swap the processes
- temp = proc[j];
- proc[j] = proc[j + 1];
- proc[j + 1] = temp;
- } } }
- int total_waiting_time = 0;
- int total_turnaround_time = 
-proc[0].burst_time;
- proc[0].waittime=0;
- 
- for (i = 1; i < n; i++) {
- proc[i].waittime=proc[i1].waittime+proc[i-1].burst_time;
- 
-total_turnaround_time+=proc[i].waittime+pro
-c[i].burst_time;
- total_waiting_time+=proc[i].waittime;
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+ int pid;
+ int btime;
+ int wtime;
+ int tatime;
+}sp;
+int main(){
+ int i, j, n;
+ float total_waiting_time=0, total_turnaround_time=0;
+ sp *p,t;
+ printf("Enter the no. of processes: ");
+ scanf("%d",&n);
+ p=(sp*)malloc(n*sizeof(sp));
+ printf("Enter the burst time of");
+ for(i=0;i<n;i++){
+ printf("\nProcess%d\t",i+1);
+ scanf("%d",&p[i].btime);
+ p[i].pid = i+1;
  }
- printf("Process\tBurst Time\tWaiting Time 
-\t Turn around time\n");
- for (i = 0; i < n; i++) {
- printf("%d\t%d\t\t\t%d\t%d\n", 
-proc[i].pid, 
-proc[i].burst_time,proc[i].waittime,proc[i].wai
-ttime+proc[i].burst_time);
+ for(i=0;i<n;i++){
+ for(j=0;j<n-i-1;j++){
+ if(p[j].btime>p[j+1].btime){
+ t=p[j];
+ p[j]=p[j+1];
+ p[j+1]=t;
  }
- printf("\nAverage Waiting Time: %.2f\n", 
-(float) total_waiting_time / n);
- printf("Average Turnaround Time: %.2f\n", 
-(float) total_turnaround_time / n);
-}
-void main() {
- int n, i;
- printf("Enter the number of processes: ");
- scanf("%d", &n);
-struct Process proc[n];
- printf("Enter process details (Process ID, 
-Burst Time):\n");
- for (i = 0; i < n; i++) {
- printf("Process %d: ", i + 1);
- scanf("%d %d", &proc[i].pid, 
-&proc[i].burst_time);
  }
- sjfScheduling(proc, n);
+ } 
+ p[0].wtime = 0;
+ for(i=1;i<n;i++){
+ p[i].wtime=0;
+ for(j=0;j<i;j++){
+ p[i].wtime+=p[j].btime;
+ }
+ total_waiting_time+=p[i].wtime;
+ }
+ printf("\n|-----------------------Process Scheduling-----------------------|");
+ printf("\n|Process\t|Burst Time\t|Waiting Time\t|Turn Around Time|");
+ printf("\n|----------------------------------------------------------------|");
+ for(i=0;i<n;i++){
+ p[i].tatime = p[i].wtime + p[i].btime;
+ total_turnaround_time += p[i].tatime;
+ printf("\n|%d\t\t|%d",p[i].pid,p[i].btime);
+ printf("\t\t|%d\t\t|%d\t\t |",p[i].wtime,p[i].tatime);
+ }
+ printf("\n|----------------------------------------------------------------|");
+ printf("\n Total Waiting Time: %.2f", total_waiting_time);
+ printf("\n Average Waiting Time: %.2f", (float)total_waiting_time / n);
+ printf("\n Total Turn Around Time: %.2f", total_turnaround_time);
+ printf("\n Average Turn Around Time: %.2f", (float)total_turnaround_time / n);
 }
