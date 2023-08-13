e2f41ca7511bc5236695a7bfbc69e5956708a18f
MANIKANDAN S <102538684+manikandans007@users.noreply.github.com>
2023-08-13 22:28:26 +0530

    Update firstfit.c

--- firstfit.c
+++ firstfit.c
@@ -1,37 +1,44 @@
-#include<stdio.h>
-void main()
-{
-	int bsize[10], psize[10], bno, pno, flags[10], allocation[10], i, j;
-	for(i = 0; i < 10; i++)
-	{
-		flags[i] = 0;
-		allocation[i] = -1;
-	}
-	printf("Enter no. of blocks: ");
-	scanf("%d", &bno);
-	printf("\nEnter size of each block: ");
-	for(i = 0; i < bno; i++)
-		scanf("%d", &bsize[i]);
-	printf("\nEnter no. of processes: ");
-	scanf("%d", &pno);
-	printf("\nEnter size of each process: ");
-	for(i = 0; i < pno; i++)
-		scanf("%d", &psize[i]);
-	for(i = 0; i < pno; i++)        
-		for(j = 0; j < bno; j++)
-			if(flags[j] == 0 && bsize[j] >= psize[i])
-			{
-				allocation[j] = i;
-				flags[j] = 1;
-				break;
-			}
-	printf("\nBlock no.\tsize\t\tprocess no.\t\tsize");
-	for(i = 0; i < bno; i++)
-	{
-		printf("\n%d\t\t%d\t\t", i+1, bsize[i]);
-		if(flags[i] == 1)
-			printf("%d\t\t\t%d",allocation[i]+1,psize[allocation[i]]);
-		else
-			printf("Not allocated");
-	}
+#include <stdio.h>
+void firstFit(int blockSize[], int m, int processSize[], int n){
+ int i,j;
+ int allocation[n];
+ for(i=0; i<n; i++){
+ allocation[i] = -1;
+ }
+ for(i=0; i<n; i++){
+ for(j=0; j<m; j++){
+ if(blockSize[j] >= processSize[i]){
+ allocation[i] = j;
+ blockSize[j] -= processSize[i];
+ break;
+ }
+ }
+ }
+ printf("\nProcess No\tProcess Size\tBlock No.\n");
+ for(i=0; i<n; i++){
+ printf("%d\t\t",i+1);
+ printf("%d\t\t",processSize[i]);
+ if(allocation[i] != -1){
+ printf("%i",allocation[i]+1);
+ }else{
+ printf("Not Allocated");
+ }printf("\n");
+ }
+}
+int main(){
+ int m,n;
+ int blockSize[5];
+ int processSize[4];
+ printf("Enter the block's size: ");
+ for(int i =0; i<5; i++){
+ scanf("%d",&blockSize[i]);
+ }
+ printf("Enter the process's Size: ");
+ for(int i =0; i<4; i++){
+ scanf("%d",&processSize[i]);
+ }
+ m = sizeof(blockSize)/sizeof(blockSize[0]);
+ n = sizeof(processSize)/sizeof(processSize[0]);
+ firstFit(blockSize,m,processSize,n);
+ return 0;
 }
